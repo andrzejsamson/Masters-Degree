e1a3d1cefdb4448c990f0239ade822ea0ab1eaed
andrzejsamson <andrzej.samson95@gmail.com>
2020-03-09 21:59:55 +0100

    Database and Saving Function
    Database for Searchings added.
    Function to save the searched items to database added.

---
+++ AppDataBase.sql
@@ -0,0 +1,5 @@
+CREATE TABLE WYSZUKANIA
+	(ID_wyszukania INTEGER NOT NULL,
+	fraza TEXT NOT NULL,
+	data_wyszukania DATE NOT NULL,
+	PRIMARY KEY(ID_wyszukania));
---
+++ zapisDoBazy.py
@@ -0,0 +1,22 @@
+import sqlite3
+from datetime import datetime
+
+def zapis(fraza):
+    try:
+        print("Wyszukiwana fraza " + fraza)
+        db = sqlite3.connect('gbif.db')
+        db.row_factory = sqlite3.Row
+        datetime_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        kursor = db.cursor()
+        kursor.execute(
+            """
+            INSERT INTO WYSZUKANIA (fraza, data_wyszukania)
+            VALUES (?,?)
+            """, (fraza, datetime_now))
+        db.commit()
+        db.close()
+        print("Dodano frazę do bazy danych")
+        return True
+    except:
+        print("Nie dodano, wystąpił błąd")
+        return False
