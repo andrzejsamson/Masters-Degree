bd2f04dd8bb8dacdec8e47e0fb5772ce5c147163
andrzejsamson <andrzej.samson95@gmail.com>
2020-04-24 09:57:21 +0200

    Comments to pygbifTest.py added

--- pygbifTest.py
+++ pygbifTest.py
@@ -1,17 +1,18 @@
-from pygbif import occurrences as occ
+from pygbif import occurrences as occ #pakiet do łączenia sie z bazą gbif przez język python
 import datetime
 
 def searching(name, myDate):
-    now = datetime.datetime.now().strftime("%Y-%m-%d")
+    now = datetime.datetime.now().strftime("%Y-%m-%d") #pobranie aktualnej daty
 
-    x = occ.search(q=name, eventDate=(myDate+','+now))
+    x = occ.search(q=name, eventDate=(myDate+','+now)) #wyszukanie frazy od daty do teraz
 
-    dane = (x['results'])
+    dane = (x['results']) #zapisanie danych
     limit = len(dane)
 
-    wektor = list()
+    wektor = list() #wektor przechowujacy rekordy
 
     for i in range(limit):
+        #zapisanie potrzebnych informacji z danych do list
         country = None
         scientificName = None
         eventDate = None
@@ -33,10 +34,11 @@ def searching(name, myDate):
         except:
             pass
         dataKey = dane[i]['key']
-        wektor.append((dataKey,country,scientificName,eventDate,recordedBy))
+        wektor.append((dataKey,country,scientificName,eventDate,recordedBy)) #połączenie wyników z list do jednej listy
 
-    wektor.sort(key=takeDate,reverse=True)
+    wektor.sort(key=takeDate,reverse=True) #poszeregowanie zmiennych po dacie, od daty najwcześniejszej
     return wektor
 
 def takeDate(elem):
+    #funkcja potrzebna do szeregowania rekordów po dacie
     return elem[3]
